2d1fb84cd72d3d3ce72aa613921157f11b042e71
srksbce <srk.sbce@gmail.com>
2022-10-12 18:50:58 +0530

    commit

--- HRMSProject/HRMSProjectMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HRMSProjectMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
